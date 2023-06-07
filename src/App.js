5946393426a5a541ed741260cebbe04efacfb1c7
lyoun3060 <omega3060@naver.com>
2023-06-07 11:50:01 +0900

    11

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import React,{useState, useCallback} from 'react';
+import React from 'react';
 import ItemsPage from './pages/ItemsPage';
 
 
 function App() {
-
-  const [category, setCategory] = useState('food')
-  const onSelect = useCallback(category => setCategory(category),[])
-
 
 
   return (
